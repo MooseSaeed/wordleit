6bfb0cce47cb2834f2ec748cbb4c392ff724cc6d
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-03-21 20:02:59 +0200

    Starting with markdown

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,23 @@ import { createApp } from "vue";
 import router from "./router";
 import Featurecard from "./components/Featurecard";
 import Bodycomponent from "./components/Bodycomponent";
+import { marked } from "marked";
+
+const markedMixin = {
+    methods: {
+        md: function (input) {
+            return marked(input);
+        },
+    },
+};
 
 createApp({
     components: {
         Featurecard,
         Bodycomponent,
+        marked,
     },
 })
+    .mixin(markedMixin)
     .use(router)
     .mount("#app");
--- resources/js/components/main/Article.vue
+++ resources/js/components/main/Article.vue
@@ -4,21 +4,30 @@
             <textarea
                 name="myTextArea"
                 id="myTextArea"
+                v-model="markdown"
                 class="w-full h-full p-5"
             ></textarea>
         </div>
-        <div class="bg-blue-500 flex-1 h-full p-2">
-            <textarea
-                name="outputTextArea"
-                id="outputTextArea"
-                class="w-full h-full p-5"
-            ></textarea>
-        </div>
+        <div
+            v-html="markdownToHtml"
+            class="bg-blue-500 flex-1 h-full p-2"
+        ></div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            markdown: " # Hello World ",
+        };
+    },
+    computed: {
+        markdownToHtml() {
+            return this.md(this.markdown);
+        },
+    },
+};
 </script>
 
 <style></style>
