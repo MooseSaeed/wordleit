488f75198cef26a747fbcbf8e51ad6f080b467c6
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-03-22 09:27:12 +0200

    adding animation to feature card

--- public/js/app.js
+++ public/js/app.js
@@ -20368,7 +20368,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 var _hoisted_1 = {
-  "class": "relative"
+  "class": "relative card-info"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", {
--- resources/js/components/card/Card.vue
+++ resources/js/components/card/Card.vue
@@ -11,7 +11,7 @@
             :style="cardStyle"
         >
             <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
-            <div class="relative">
+            <div class="relative card-info">
                 <slot name="header"></slot>
                 <slot name="content"></slot>
             </div>
